f6ac19f079f26ba103c929196ad3f9d598017362
Peter Kamau <kamaupeter343@gmail.com>
2025-08-18 10:31:38 +0300

    feat: Introduced a testable view from within the app

--- hooks/expense.ts
+++ hooks/expense.ts
@@ -1,55 +1,56 @@
-import { queryKeys, ReadExpenseAccounts } from "@/constants";
+import { ReadExpenseAccounts } from "@/constants";
 import { Expense } from "@/services/expense";
 import { useProfileStore } from "@/store/profile";
 import { useResultStore } from "@/store/result";
 import { useUserStore } from "@/store/user";
-import { HillFreshError } from "@/utils/customError";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export function useRetrieveExpenseAccounts() {
 	const [expenses, setExpenses] = useState<ReadExpenseAccounts[]>([]);
 	const { profile } = useProfileStore();
 	const { limit } = useResultStore();
 	const { user } = useUserStore();
-	// const [isLoading, setIsLoading] = useState<boolean>(true);
-	// // const { limit } = useResultStore();
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	// const { limit } = useResultStore();
 
-	// const [fetchTrigger, setFetchTrigger] = useState(0);
-	// const refetch = useCallback(() => {
-	// 	setIsLoading(true);
-	// 	setFetchTrigger(prevTrigger => prevTrigger + 1);
-	// }, []);
+	const [fetchTrigger, setFetchTrigger] = useState(0);
+	const refetch = useCallback(() => {
+		setIsLoading(true);
+		setFetchTrigger(prevTrigger => prevTrigger + 1);
+	}, []);
 
-	// useEffect(() => {
-	// 	const retrieveWarehouses = async () => {
-	// 		const expaccnts = await exaccounts.retrieveExpenseAccounts({
-	// 			page_length: limit * 2,
-	// 		});
-	// 		setIsLoading(false);
-	// 		setExpenses(expaccnts);
-	// 	};
-	// 	retrieveWarehouses();
-	// }, [profile, fetchTrigger]);
-
-	const { isLoading, data, error, refetch } = useQuery({
-		queryKey: [...queryKeys.expenses.all, user && user.email],
-		queryFn: async () => {
+	useEffect(() => {
+		const retrieveWarehouses = async () => {
 			const exaccounts = new Expense({
 				docType: "Account",
 			});
-			return await exaccounts.retrieveExpenseAccounts({
+			const expaccnts = await exaccounts.retrieveExpenseAccounts({
 				page_length: limit * 2,
 			});
-		},
-	});
+			setIsLoading(false);
+			setExpenses(expaccnts);
+		};
+		retrieveWarehouses();
+	}, [profile, fetchTrigger]);
 
-	if (error) {
-		throw new HillFreshError({
-			message: "Error Getting Expenses",
-			action: { label: "close" },
-		});
-	}
+	// const { isLoading, data, error, refetch } = useQuery({
+	// 	queryKey: [...queryKeys.expenses.all, user && user.email],
+	// 	queryFn: async () => {
+	// 		const exaccounts = new Expense({
+	// 			docType: "Account",
+	// 		});
+	// 		return await exaccounts.retrieveExpenseAccounts({
+	// 			page_length: limit * 2,
+	// 		});
+	// 	},
+	// });
 
-	return { refetch, expenses: data, isLoading };
+	// if (error) {
+	// 	throw new HillFreshError({
+	// 		message: "Error Getting Expenses",
+	// 		action: { label: "close" },
+	// 	});
+	// }
+
+	return { refetch, expenses, isLoading };
 }
